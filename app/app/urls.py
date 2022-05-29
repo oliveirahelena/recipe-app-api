94d17a1dfce4b8378ca1a9e8c414f05a5b645879
Helena Oliveira <helena@informach.com.br>
2022-05-29 17:29:09 -0300

    add user api

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,7 +19,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -34,6 +34,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
+    path('api/user/', include('user.urls')),
 ]
 
 if settings.DEBUG:
